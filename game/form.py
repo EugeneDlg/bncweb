09f080fee75963a497b7ec6991ed98c49801ae2b
EugeneDlg <stayerx@gmail.com>
2022-10-06 15:28:43 +0300

    20221006

--- game/form.py
+++ game/form.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
+from django.contrib.auth.forms import UserChangeForm
 from django.contrib.auth.models import User
 
 
