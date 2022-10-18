e454350c854f2eaa9125c3a2cda1b122055e0ec6
Eugene Dlg <stayerx@gmail.com>
2022-10-18 09:52:42 +0300

    20221018 users app added 3

---
+++ game/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.models import User
+
+
+class UserEditForm(UserChangeForm):
+    # def __init__(self, *args, **kwargs):
+        # super().__init__(*args, **kwargs)
+        # self.fields['password'].
+    first_name = forms.CharField(max_length=100, required=True)
+    last_name = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(max_length=250, required=True, help_text='eg. youremail@gmail.com')
+    password = forms.CharField(widget=forms.HiddenInput())
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email')
+
+
+
---
+++ users/forms.py
@@ -0,0 +1,43 @@
+from django import forms
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.contrib.auth.models import User
+
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=100, required=True)
+    last_name = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(max_length=250, required=True, help_text='eg. youremail@gmail.com')
+    avatar = forms.ImageField(max_length=950, required=False,
+                              help_text="Max. volume is 1 Mb. Max. height and width are 600px")
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'password1', 'password2', 'email', 'avatar')
+
+    def save(self):
+        user = super().save()
+        # user.first_name = self.cleaned_data['first_name']
+        # user.last_name = self.cleaned_data['last_name']
+        # user.email = self.cleaned_data['email']
+
+        # user has to be saved to add profile
+        # user.save()
+        breakpoint()
+        user.create_extension()
+        user.extension.avatar = self.cleaned_data.get('avatar')
+        user.extension.save()
+        # user.save()
+
+
+class UserEditForm(UserChangeForm):
+
+    first_name = forms.CharField(max_length=100, required=True)
+    last_name = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(max_length=250, required=True, help_text='eg. youremail@gmail.com')
+    password = forms.CharField(widget=forms.HiddenInput())
+    avatar = forms.ImageField(max_length=950, required=False,
+                              help_text="Max. volume is 1 Mb. Max. height and width are 600px")
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email', 'avatar')
